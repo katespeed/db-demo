62695d73a4604a11bcd1d1c43185262091fccb3e
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2023-03-14 12:22:06 -0500

    Completed

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -1,32 +1,31 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    //   ManyToOne,
-    ManyToMany,
-    Relation,
-    OneToMany,
-    JoinTable,
-  } from 'typeorm';
-  import { Review } from './Review';
-  import { Author } from './Author';
-  
-  @Entity()
-  export class Book {
-    @PrimaryGeneratedColumn('uuid')
-    bookiId: string;
-  
-    @Column()
-    title: string;
-  
-    @Column({ nullable: true })
-    pubkicationYear: number;
-  
-    @OneToMany(() => Review, (review) => review.user)
-    reviews: Relation<Review>[];
-  
-    @ManyToMany(() => Author, (author) => author.book)
-    @JoinTable()
-    author: Relation<Author>[];
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  //   ManyToOne,
+  ManyToMany,
+  Relation,
+  OneToMany,
+  JoinTable,
+} from 'typeorm';
+import { Review } from './Review';
+import { Author } from './Author';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  bookiId: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  pubkicationYear: number;
+
+  @OneToMany(() => Review, (review) => review.user)
+  reviews: Relation<Review>[];
+
+  @ManyToMany(() => Author, (author) => author.book)
+  @JoinTable()
+  author: Relation<Author>[];
+}
