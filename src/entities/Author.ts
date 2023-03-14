80fc308025e7439f8553c33334d5e2f9b1b310fe
Keito Yamakawa <keito.yamakawa@smail.astate.edu>
2023-03-14 12:20:11 -0500

    Complated

--- src/entities/Author.ts
+++ src/entities/Author.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, Relation } from 'typeorm';
 import { Book } from './Book';
-
+//
 @Entity()
 export class Author {
   @PrimaryGeneratedColumn('uuid')
